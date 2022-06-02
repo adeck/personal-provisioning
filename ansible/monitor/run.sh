77f123f62fdfb6e65aed7d00aa4249a023346812
garin <nope@nope>
2022-06-01 17:41:35 -0700

    switched from plaintext password files to vault vars for simplicity

--- ansible/monitor/run.sh
+++ ansible/monitor/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-ansible-playbook -v -i inventory.yaml $1
+ansible-playbook -v \
+    -i inventory.yaml \
+    --vault-id $CUR_ENV@prompt \
+    $1
 
--- ansible/monitor/vault.sh
+++ ansible/monitor/vault.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -x
-CUR_ENV=devel
-ansible-vault create --vault-id $CUR_ENV@prompt \
+ansible-vault edit --vault-id $CUR_ENV@prompt \
         nogit/vaults/$CUR_ENV-vault.yaml
 
